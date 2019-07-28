469e32dc0f384d26b757775c26ee9c8ed4978189
HarshitChhipa <harshitkankerwal@gmail.com>
2019-07-28 22:46:43 +0530

    added cart and dashboard and Layout of the application

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './assets/App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container-fluid">
+            <div className="jumbotron">
+                <h1>Order & Out</h1>
+                <p>A web application that allows users to order food.</p>
+            </div>
+            {/*<Dashboard />*/}
+        </div>
+    );
 }
 
 export default App;
